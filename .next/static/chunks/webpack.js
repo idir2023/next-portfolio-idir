9daa328c1df1646f4fbaad0839d295b349ed9bf7
lahcen idir <128367702+idir2023@users.noreply.github.com>
2024-09-23 17:53:27 +0100

    fix_img

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "59b5d997d5609980"; }
+/******/ 		__webpack_require__.h = function() { return "a2dc91b554f549aa"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
