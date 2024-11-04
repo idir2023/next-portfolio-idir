f672b2760086e66c6f82450b601ddd404c20ce25
lahcen idir <128367702+idir2023@users.noreply.github.com>
2024-11-04 16:21:16 +0100

    img_3

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  images: {
-    domains: ['firebasestorage.googleapis.com', 'another-image-host.com'], // Allow images from these domains
-    unoptimized: true, // Enable unoptimized images for development or specific cases
-  },
   basePath: '/next-portfolio-idir',
   assetPrefix: '/next-portfolio-idir',
+  images: {
+    domains: ['firebasestorage.googleapis.com'], // add external domains if needed
+    unoptimized: true, // Disable Next.js image optimization for static hosting
+  },
 };
