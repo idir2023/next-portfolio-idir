894fde4ba2ec86e8c8cde71920652fd19790a4db
lahcen idir <128367702+idir2023@users.noreply.github.com>
2025-03-26 17:51:42 +0100

    new add

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
-// module.exports = {
-//   basePath: '/next-portfolio-idir',
-//   assetPrefix: '/next-portfolio-idir',
-//   images: {
-//     domains: ['firebasestorage.googleapis.com'], // add external domains if needed
-//   },
-// };
 module.exports = {
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/home",
-        permanent: true,
-      },
-    ];
+  basePath: '/next-portfolio-idir',
+  assetPrefix: '/next-portfolio-idir',
+  images: {
+    domains: ['firebasestorage.googleapis.com'], // add external domains if needed
   },
 };
+// module.exports = {
+//   async redirects() {
+//     return [
+//       {
+//         source: "/",
+//         destination: "/home",
+//         permanent: true,
+//       },
+//     ];
+//   },
+// };
