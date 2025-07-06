c2108355b740c8ffcf8638309868e7274f6fab92
lahcen idir <128367702+idir2023@users.noreply.github.com>
2025-07-06 16:45:32 +0100

    commit now 16.45

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "83dd4e5a0bc5116e"; }
+/******/ 		__webpack_require__.h = function() { return "2aa279840a263e36"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
